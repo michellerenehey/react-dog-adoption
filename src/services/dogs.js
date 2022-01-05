506e4b54190570715fb5225e680470d581aecd07
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-01-05 15:12:20 -0800

    updateDog function in services folder

--- src/services/dogs.js
+++ src/services/dogs.js
@@ -9,3 +9,8 @@ export async function fetchDogById(id) {
   const response = await client.from('dogs').select('*').match({ id }).single();
   return checkError(response);
 }
+
+export async function updateDog(id, name, breed, image, bio) {
+  const response = await client.from('dogs').update(name, breed, image, bio).eq('id', id);
+  return checkError(response);
+}
